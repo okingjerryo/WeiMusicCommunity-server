1f3aea9b6b3b6bebef0520caafdcdea3f8e9d357
UryuoHamusta <uryuohamusta@gmail.com>
2017-04-29 16:16:13 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/weiCommity/Util/StaticVar.java
+++ src/main/java/com/weiCommity/Util/StaticVar.java
@@ -4,9 +4,5 @@ package com.weiCommity.Util;
  * Created by uryuo on 17/4/24.
  */
 public class StaticVar {
-   public static String preFilePath = "../../FileSpace/WeiCommity";
 
-    public static String getPreFilePath() {
-        return preFilePath;
-    }
 }
